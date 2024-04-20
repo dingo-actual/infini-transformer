f2bf086b3ef443add9bff601cec0536b7856ac10
Ryan Taylor <ryan@beta-reduce.net>
2024-04-20 11:59:39 -0400

    rename from decoder to infinitransformer; added causal masking

--- infini_transformer/infini_transformer.py
+++ infini_transformer/infini_transformer.py
@@ -1,11 +1,13 @@
+from typing import Optional
+
 import torch
 from torch import nn
 
 from compressive_memory import CompressiveMemory
 
 
-class Decoder(nn.Module):
-    """Transformer decoder layer with compressive memory."""
+class InfiniTransformer(nn.Module):
+    """Transformer layer with compressive memory."""
     def __init__(self, dim_input: int, dim_hidden: int, dim_key: int, dim_value: int, num_heads: int, segment_len: int, update="linear", dropout: float = 0.0):
         """Initializes the module.
 
@@ -19,7 +21,7 @@ class Decoder(nn.Module):
             update (str, optional): Type of memory update rule to use for the CompressiveMemory ("linear" or "delta"). Defaults to "linear".
             dropout (float, optional): Dropout rate for the MLP. Defaults to 0.0.
         """
-        super(Decoder, self).__init__()
+        super(InfiniTransformer, self).__init__()
         
         # Multi-head attention
         self.attn = CompressiveMemory(dim_input, dim_key, dim_value, num_heads, segment_len, update)
@@ -33,18 +35,18 @@ class Decoder(nn.Module):
         )
         self.layer_norm = nn.LayerNorm(dim_input)
         
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
         """Forward pass.
 
         Args:
             x (torch.Tensor): Input tensor of shape (batch_size, seq_len, dim_input).
-
+            mask (torch.Tensor, optional): Attention mask of shape (seq_len, seq_len). Defaults to None.
         Returns:
             torch.Tensor: Output tensor of shape (batch_size, seq_len, dim_input).
         """
         
         # Apply multi-head attention, followed by MLP and layer normalization with residual connection.
-        x_ = self.attn(x)
+        x_ = self.attn(x, mask)
         x_ = self.mlp(x_)
         
         return self.layer_norm(x_ + x)
