931baed1f9830a067ce44b0e5a0332ebfc4017ae
Ryan Taylor <ryan@beta-reduce.net>
2024-04-21 16:07:59 -0400

    replace errors with warnings for short sequences and indivisible sequences

--- infini_transformer/compressive_memory.py
+++ infini_transformer/compressive_memory.py
@@ -1,4 +1,5 @@
 from typing import Optional
+import warnings
 
 import torch
 from torch import nn
@@ -54,10 +55,14 @@ class CompressiveMemory(nn.Module):
         batch_size, seq_len, _ = x.shape
 
         num_segments, rem = divmod(seq_len, self.segment_len)
+        use_full_attn = False
 
         if rem != 0:
-            raise ValueError(
-                "Sequence length must be divisible by segment length.")
+            warnings.warn(f"Sequence length ({seq_len}) is not divisible by segment length ({self.segment_len}). Falling back to full attention.")
+            use_full_attn = True
+        if seq_len < self.segment_len:
+            warnings.warn(f"Sequence length ({seq_len}) is less than segment length ({self.segment_len}). Falling back to full attention.")
+            use_full_attn = True
 
         out = []
 
@@ -68,64 +73,80 @@ class CompressiveMemory(nn.Module):
         
         # Project the input tensor to get the key, value, and query tensors
         k_full = self.proj_k(x).unsqueeze(1).view(
-            (batch_size, self.num_heads, self.segment_len * num_segments, self.dim_key))
+            (batch_size, self.num_heads, x.size(1), self.dim_key))
         v_full = self.proj_v(x).unsqueeze(1).view(
-            (batch_size, self.num_heads, self.segment_len * num_segments, self.dim_value))
+            (batch_size, self.num_heads, x.size(1), self.dim_value))
         q_full = self.proj_q(x).unsqueeze(1).view(
-            (batch_size, self.num_heads, self.segment_len * num_segments, self.dim_key))
+            (batch_size, self.num_heads, x.size(1), self.dim_key))
         
-        # If attention mask is given, resize it to match the segment tensors
-        if mask is not None:
-            mask = mask.unsqueeze(0).unsqueeze(0).repeat((batch_size, self.num_heads, 1, 1))
-
-        for ix in range(num_segments):
-            ix_lo = ix * self.segment_len
-            ix_hi = ix_lo + self.segment_len
-
-            # Extract segment from key, value and query tensors
-            k = k_full[:, :, ix_lo:ix_hi, :]
-            v = v_full[:, :, ix_lo:ix_hi, :]
-            q = q_full[:, :, ix_lo:ix_hi, :]
-            
-            # Pre-calculate sigma(q) for updating memory and calculating attention
-            # shape: (batch_size, num_heads, segment_len, dim_key)
-            sigma_q = (nn.functional.elu(q) + 1.0)
-
-            # Apply normalization term update
-            z = z + (nn.functional.elu(k) + 1.0).sum(dim=-2, keepdim=True)
-
-            # Apply SDP attention
-            scores = q @ k.transpose(-2, -1) / torch.sqrt(torch.tensor(self.dim_key))
+        # If flag is set to fall back to full attention, calculate full SDP attention
+        if use_full_attn:
+            scores = q_full @ k_full.transpose(-2, -1) / self.dim_key ** 0.5
+            # If attention mask is given, apply it to the scores, based on dtype
             if mask is not None:
+                mask = mask.unsqueeze(0).unsqueeze(0).repeat((batch_size, self.num_heads, 1, 1))
                 if mask.dtype == torch.bool:
                     scores.masked_fill_(torch.logical_not(mask), float('-inf'))
                 else:
                     scores += mask.view((batch_size, self.num_heads, seq_len, seq_len))
-            att_dot = nn.functional.softmax(scores, dim=-1) @ v
+            
+            # Return SDP attention
+            out = torch.nn.functional.softmax(scores, dim=-1) @ v_full
+        else:
+            # If attention mask is given, resize it to match the segment tensors
+            if mask is not None:
+                mask = mask.unsqueeze(0).unsqueeze(0).repeat((batch_size, self.num_heads, 1, 1))
 
-            # Calculate normalized linear attention
-            # shape: (batch_size, num_heads, segment_len, dim_value)
-            att_mem = (sigma_q @ mem) / (sigma_q @ z)
+            for ix in range(num_segments):
+                ix_lo = ix * self.segment_len
+                ix_hi = ix_lo + self.segment_len
 
-            # Apply mem update
-            sigma_k = nn.functional.elu(k) + 1.0
-            if self.update == "linear":
-                mem = mem + sigma_k.transpose(-2, -1) @ v
-            elif self.update == "delta":
-                mem = mem + \
-                    sigma_k.transpose(-2, -1) @ (v - (sigma_k @ mem) / (sigma_k @ z))
+                # Extract segment from key, value and query tensors
+                k = k_full[:, :, ix_lo:ix_hi, :]
+                v = v_full[:, :, ix_lo:ix_hi, :]
+                q = q_full[:, :, ix_lo:ix_hi, :]
+                
+                # Pre-calculate sigma(q) for updating memory and calculating attention
+                # shape: (batch_size, num_heads, segment_len, dim_key)
+                sigma_q = (nn.functional.elu(q) + 1.0)
 
-            # Calculate weighted average of dot-product and memory-based attention
-            att = nn.functional.sigmoid(
-                self.betas) * att_mem + (1 - nn.functional.sigmoid(self.betas)) * att_dot
-            att = att.view((batch_size, self.segment_len,
-                           self.num_heads * self.dim_value))
+                # Apply normalization term update
+                z = z + (nn.functional.elu(k) + 1.0).sum(dim=-2, keepdim=True)
 
-            # Append output to buffer
-            out.append(self.proj_out(att))
+                # Apply SDP attention
+                scores = q @ k.transpose(-2, -1) / torch.sqrt(torch.tensor(self.dim_key))
+                if mask is not None:
+                    if mask.dtype == torch.bool:
+                        scores.masked_fill_(torch.logical_not(mask), float('-inf'))
+                    else:
+                        scores += mask.view((batch_size, self.num_heads, seq_len, seq_len))
+                att_dot = nn.functional.softmax(scores, dim=-1) @ v
 
-        # Return concatenated full sequence from buffer
-        return torch.concat(out, dim=1)
+                # Calculate normalized linear attention
+                # shape: (batch_size, num_heads, segment_len, dim_value)
+                att_mem = (sigma_q @ mem) / (sigma_q @ z)
+
+                # Apply mem update
+                sigma_k = nn.functional.elu(k) + 1.0
+                if self.update == "linear":
+                    mem = mem + sigma_k.transpose(-2, -1) @ v
+                elif self.update == "delta":
+                    mem = mem + \
+                        sigma_k.transpose(-2, -1) @ (v - (sigma_k @ mem) / (sigma_k @ z))
+
+                # Calculate weighted average of dot-product and memory-based attention
+                att = nn.functional.sigmoid(
+                    self.betas) * att_mem + (1 - nn.functional.sigmoid(self.betas)) * att_dot
+                att = att.view((batch_size, self.segment_len,
+                            self.num_heads * self.dim_value))
+
+                # Append output to buffer
+                out.append(self.proj_out(att))
+
+            # Return concatenated full sequence from buffer
+            out = torch.concat(out, dim=1)
+
+        return out
 
 
 def demo_compressive_memory():
@@ -144,7 +165,25 @@ def demo_compressive_memory():
     batch = torch.randn(4, 128, dim_input)
     mask = torch.tril(torch.ones(segment_len, segment_len), diagonal=0).bool()
     model(batch, mask)
+    
+    
+def demo_compressive_memory_short_seq():
+    # Example usage
+    dim_input = 512
+    dim_key = 64
+    dim_value = 64
+    num_heads = 8
+    segment_len = 32
+    update = "linear"
+
+    model = CompressiveMemory(
+        dim_input, dim_key, dim_value, num_heads, segment_len, update)
+
+    # Generate some random input
+    batch = torch.randn(4, 16, dim_input)
+    model(batch)
 
 
 if __name__ == "__main__":
     demo_compressive_memory()
+    demo_compressive_memory_short_seq()
