a8f7e2f1faae0bbaa924b1159fffdaa5e387d6b4
Ryan Taylor <ryan@beta-reduce.net>
2024-05-02 08:47:35 -0400

    refactored sin and cos component calculations to eliminate duplicate storage

--- infini_transformer/positional_embeddings.py
+++ infini_transformer/positional_embeddings.py
@@ -31,8 +31,8 @@ class RoPEEmbeddings(nn.Module):
         self.base = base
         self.last_offset = 0
         
-        # Initialize cos and sin component matrices
-        self._calculate_cos_sin_components()
+        # Initialize theta matrix
+        self._calculate_thetas()
         
         # Initialize sin component indices for input tensor
         # Indices for rearranging the input follow the pattern [1, 0, 3, 2, 5, 4, ...]
@@ -42,7 +42,7 @@ class RoPEEmbeddings(nn.Module):
         self.ixs_sin[self.ixs_sin_neg] = self.ixs_sin_neg + 1
         self.ixs_sin[self.ixs_sin_neg + 1] = self.ixs_sin_neg
         
-    def _calculate_cos_sin_components(self, offset: int = 0, select_mask: Optional[torch.Tensor] = None) -> None:
+    def _calculate_thetas(self, offset: int = 0, select_mask: Optional[torch.Tensor] = None) -> None:
         """Calculate the cosine and sine component matrices for the rotary positional embeddings.
         Uses multidimensional extension of theta as defined in Sec 3.2.2 as well as equation (34)
         from the RoFormer paper
@@ -60,12 +60,7 @@ class RoPEEmbeddings(nn.Module):
             )
             # Multiply by index positions, then transpose to get correct shape
             thetas *= torch.arange(1 + offset, self.seq_len + 1 + offset).unsqueeze(0)
-            thetas = thetas.transpose(0, 1)
-            
-            # Calculate cosine and sine of thetas and reshape for downstream use
-            # Shape: (1, 1, seq_len, effective_dim)
-            self.cos_component = thetas.cos().unsqueeze(0).unsqueeze(0)
-            self.sin_component = thetas.sin().unsqueeze(0).unsqueeze(0)
+            self.thetas = thetas.transpose(0, 1).unsqueeze(0).unsqueeze(0)
         else:
             # (n_obs, select_seq_len)
             select_ixs = 1 + offset + torch.argwhere(select_mask)[:, 1].view((select_mask.size(0), -1))
@@ -80,10 +75,7 @@ class RoPEEmbeddings(nn.Module):
             # (n_obs, select_seq_len, effective_dim)
             thetas = thetas.transpose(0, 1).unsqueeze(0).repeat((select_mask.size(0), 1, 1))
             thetas *= select_ixs
-            
-            # Calculate cosine and sine of thetas and reshape for downstream use
-            self.cos_component = thetas.cos().unsqueeze(1)
-            self.sin_component = thetas.sin().unsqueeze(1)
+            self.thetas = thetas.unsqueeze(1)
         
     def forward(self, x: torch.Tensor, offset: int = 0, select_mask: Optional[torch.Tensor] = None) -> torch.Tensor:
         """Applies rotary positional embeddings to the input tensor. Uses a multidimensional
@@ -98,7 +90,7 @@ class RoPEEmbeddings(nn.Module):
             torch.Tensor: Transformed input tensor with rotary positional embeddings applied.
         """
         if offset != self.last_offset:
-            self._calculate_cos_sin_components(offset=offset, select_mask=select_mask)
+            self._calculate_thetas(offset=offset, select_mask=select_mask)
             self.last_offset = offset
             cos_sin_recalculated = True
         else:
@@ -115,25 +107,25 @@ class RoPEEmbeddings(nn.Module):
             # If the sequence length is less than the maximum sequence length, perform calculations
             # with truncated cos_component and sin_component, along the sequence axis
             if x.size(2) < self.seq_len:
-                x_cos = self.cos_component[:, :, :x_pos.size(2), :].repeat(x_pos.size(0), x_pos.size(1), 1, 1) * x_pos
+                x_cos = self.thetas.cos()[:, :, :x_pos.size(2), :].repeat(x_pos.size(0), x_pos.size(1), 1, 1) * x_pos
                 x_sin = x_pos[..., self.ixs_sin]
                 x_sin[..., self.ixs_sin_neg] = -x_sin[...,self.ixs_sin_neg]
-                x_sin *= self.sin_component[:, :, :x_pos.size(2), :].repeat(x_pos.size(0), x_pos.size(1), 1, 1)
+                x_sin *= self.thetas.sin()[:, :, :x_pos.size(2), :].repeat(x_pos.size(0), x_pos.size(1), 1, 1)
             # Otherwise, perform calculations with the full cos_component and sin_component
             else:
-                x_cos = self.cos_component.repeat(x_pos.size(0), x_pos.size(1), 1, 1) * x_pos
+                x_cos = self.thetas.cos().repeat(x_pos.size(0), x_pos.size(1), 1, 1) * x_pos
                 x_sin = x_pos[..., self.ixs_sin]
                 x_sin[..., self.ixs_sin_neg] = -x_sin[...,self.ixs_sin_neg]
-                x_sin *= self.sin_component.repeat(x_pos.size(0), x_pos.size(1), 1, 1)
+                x_sin *= self.thetas.sin().repeat(x_pos.size(0), x_pos.size(1), 1, 1)
         # If a selection mask is specified, incorporate it into the positional embeddings
         else:
             if not cos_sin_recalculated:
-                self._calculate_cos_sin_components(offset=offset, select_mask=select_mask)
+                self._calculate_thetas(offset=offset, select_mask=select_mask)
                 self.last_offset = offset
-            x_cos = self.cos_component.repeat(1, x_pos.size(1), 1, 1) * x_pos
+            x_cos = self.thetas.cos().repeat(1, x_pos.size(1), 1, 1) * x_pos
             x_sin = x_pos[..., self.ixs_sin]
             x_sin[..., self.ixs_sin_neg] = -x_sin[...,self.ixs_sin_neg]
-            x_sin *= self.sin_component.repeat(1, x_pos.size(1), 1, 1)
+            x_sin *= self.thetas.sin().repeat(1, x_pos.size(1), 1, 1)
             
         # If the sequence length is less than the maximum sequence length, concatenate positionally embedded
         # entries with original entries, otherwise return the positionally embedded entries
